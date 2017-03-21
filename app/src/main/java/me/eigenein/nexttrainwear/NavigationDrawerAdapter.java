22a0115408c45f4b750cfeceb7b332be88a43c45
Pavel Perestoronin <eigenein@gmail.com>
2017-03-21 01:12:38 +0100

    Added icons. Changed layout. Added empty adapter.

---
+++ app/src/main/java/me/eigenein/nexttrainwear/NavigationDrawerAdapter.java
@@ -0,0 +1,30 @@
+package me.eigenein.nexttrainwear;
+
+import android.graphics.drawable.Drawable;
+import android.support.wearable.view.drawer.WearableNavigationDrawer.WearableNavigationDrawerAdapter;
+
+public class NavigationDrawerAdapter extends WearableNavigationDrawerAdapter {
+
+    @Override
+    public String getItemText(final int index) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public Drawable getItemDrawable(final int index) {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void onItemSelected(final int index) {
+        // TODO
+    }
+
+    @Override
+    public int getCount() {
+        // TODO
+        return 0;
+    }
+}
