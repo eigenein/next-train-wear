b0414d73c7a407d9a2f5cb5912d881206f0b3432
Pavel Perestoronin <eigenein@gmail.com>
2017-05-29 23:48:33 +0200

    Fix negative minutes and seconds

--- app/src/main/java/me/eigenein/nexttrainwear/Extensions.kt
+++ app/src/main/java/me/eigenein/nexttrainwear/Extensions.kt
@@ -7,15 +7,15 @@ operator fun Date.minus(other: Date): Long {
 }
 
 fun Long.toHmDurationString(): String {
-    return String.format("%d:%02d", this / 3600000, (this % 3600000) / 60000)
+    return String.format("%d:%02d", this / 3600000, Math.abs((this % 3600000) / 60000))
 }
 
 fun Long.toHmsDurationString(): String {
     val hours = this / 3600000
     val minutes = (this % 3600000) / 60000
-    val seconds = (this % 60000) / 1000
+    val seconds = Math.abs((this % 60000) / 1000)
     if (hours == 0L) {
         return String.format("%d:%02d", minutes, seconds)
     }
-    return String.format("%d:%02d:%02d", hours, minutes, seconds)
+    return String.format("%d:%02d:%02d", hours, Math.abs(minutes), seconds)
 }
